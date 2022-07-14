b06cead1348c4c53a4b38884585130f1f92f5ee7
mhdelavega <huertadelavegamiguel@gmail.com>
2022-07-14 17:59:16 +0200

    FEAT: fetch axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,42 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 
+const axios = require('axios').default;
+
+
 const BASE_URL = "http://localhost:3001/books";
 
 function App() {
 
   const [booksList, setBooksList] = useState([]);
 
+
+
   useEffect(() => {
-    fetch(BASE_URL)
-    .then(res => res.json())
-    .then( res => {
-      setBooksList(res);
-      console.log(res);
-    })
-    .catch(err => {console.log(err)});
-  
+    // fetch(BASE_URL)
+    // .then(res => res.json())
+    // .then( res => {
+    //   setBooksList(res);
+    //   console.log(res);
+    // })
+    // .catch(err => {console.log(err)});
+    const getBooks = async () => {
+      try {
+        const res = await axios.get(BASE_URL);
+        setBooksList(res.data);
+        console.log(res);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    getBooks();
   }, []);
   
   const Books = () => {
-    if (booksList) {
+    if (booksList.length) {
       return (
           booksList.map(e => 
-            <p key={e.createdAt}>{e.author}</p>            
+            <p key={e._id}>{e.author}</p>            
           )
         );
       } else {
